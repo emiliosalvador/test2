4d9a5e0f9cbdffcef38eedf3d1a863a923db3936
emiliosalvador <emiliosalvador@hotmail.com>
2018-02-06 14:12:09 -0800

    sd

--- test2/lambda.js
+++ test2/lambda.js
@@ -8,7 +8,7 @@ exports.handler = function (event, context, callback) {
 			console.log(data);           // successful response
 			/*
 			data = {
-				LocationConstraint: "us-west-2 o lo que sea"
+				LocationConstraint: "us-west-2 test2"
 			}
 			*/
 		})
